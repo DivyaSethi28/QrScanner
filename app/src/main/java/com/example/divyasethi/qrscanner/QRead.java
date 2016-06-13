c6bd1b214a465c434d51f6e100658327c31b6333
divyasethi <divseth5@gmail.com>
2016-06-13 14:44:21 +0530

    second commit

--- app/src/main/java/com/example/divyasethi/qrscanner/QRead.java
+++ app/src/main/java/com/example/divyasethi/qrscanner/QRead.java
@@ -2,12 +2,48 @@ package com.example.divyasethi.qrscanner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+import github.nisrulz.qreader.QRDataListener;
+import github.nisrulz.qreader.QREader;
 
 public class QRead extends AppCompatActivity {
+
+    private SurfaceView surfaceView;
+    private QREader qrEader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qread);
+        surfaceView=(SurfaceView)findViewById(R.id.surface);
+        qrEader=new QREader.Builder(this, surfaceView, new QRDataListener() {
+            @Override
+            public void onDetected(String data) {
+
+                Toast.makeText(getApplicationContext(),data,Toast.LENGTH_SHORT).show();
+
+            }
+        }).facing(QREader.BACK_CAM).enableAutofocus(true).height(300).width(300).build();
+
+        qrEader.init();
+
+
+    }
+
+    @Override
+    protected void onStart(){
+
+        super.onStart();
+        qrEader.start();
+    }
+
+    @Override
+    protected void onDestroy(){
+
+        super.onDestroy();
+        qrEader.releaseAndCleanup();
+
     }
 }
--- app/src/main/java/com/example/divyasethi/qrscanner/ZbarAct.java
+++ app/src/main/java/com/example/divyasethi/qrscanner/ZbarAct.java
@@ -1,13 +1,108 @@
 package com.example.divyasethi.qrscanner;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.List;
+import me.dm7.barcodescanner.zbar.BarcodeFormat;
+import me.dm7.barcodescanner.zbar.Result;
+import me.dm7.barcodescanner.zbar.ZBarScannerView;
 
-public class ZbarAct extends AppCompatActivity {
+public class ZbarAct extends AppCompatActivity implements ZBarScannerView.ResultHandler {
+
+    private ZBarScannerView zBarScannerView;
+    private boolean isFlash=false;
+    private ImageView imageView;
+    List<BarcodeFormat> formatList=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zbar);
+        FrameLayout contentFrame= (FrameLayout) findViewById(R.id.content_frame);
+        zBarScannerView=new ZBarScannerView(this);
+        contentFrame.addView(zBarScannerView);
+        imageView= (ImageView)findViewById(R.id.scan_tick);
+      /*  Button qr= (Button)findViewById(R.id.qr);
+        Button bar=(Button)findViewById(R.id.bar);
+        Button flash= (Button)findViewById(R.id.flash);
+
+
+
+
+        qr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                formatList=new ArrayList<BarcodeFormat>();
+                formatList.add(BarcodeFormat.QRCODE);
+                zBarScannerView.setFormats(formatList);
+                imageView.setVisibility(View.INVISIBLE);
+                zBarScannerView.startCamera();
+
+            }
+        });
+
+        bar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                formatList=new ArrayList<BarcodeFormat>();
+                formatList.add(BarcodeFormat.CODE128);
+                zBarScannerView.setFormats(formatList);
+                imageView.setVisibility(View.INVISIBLE);
+                zBarScannerView.startCamera();
+            }
+        });
+
+
+        flash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            @TargetApi(Build.VERSION_CODES.M)
+            public void onClick(View v) {
+                    isFlash=!isFlash;
+                    zBarScannerView.setFlash(isFlash);
+                }
+
+            }); */
+
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        zBarScannerView.setResultHandler(this);
+        zBarScannerView.startCamera();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        zBarScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        zBarScannerView.stopCamera();
+    if(result!=null){
+
+        imageView.setVisibility(View.VISIBLE);
+        imageView.bringToFront();
+        Toast.makeText(this,result.getContents(),Toast.LENGTH_LONG).show();
+
+    }
+        else
+        {
+            Toast.makeText(this,"Null Result",Toast.LENGTH_LONG).show();
+        }
+
+
     }
 }
