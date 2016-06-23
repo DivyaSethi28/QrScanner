3d0fa6ea79a814c269f3160ad714534796e1e5f5
divyasethi <divya.sethi@gdn-commerce.com>
2016-06-23 15:23:15 +0530

    commit

--- app/src/main/java/com/example/divyasethi/qrscanner/ZbarAct.java
+++ app/src/main/java/com/example/divyasethi/qrscanner/ZbarAct.java
@@ -1,67 +1,205 @@
 package com.example.divyasethi.qrscanner;
 
+import android.Manifest;
 import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
+import android.os.Handler;
+import android.provider.Settings;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
-import java.util.ArrayList;
-import java.util.List;
-import me.dm7.barcodescanner.zbar.BarcodeFormat;
 import me.dm7.barcodescanner.zbar.Result;
 import me.dm7.barcodescanner.zbar.ZBarScannerView;
 
 public class ZbarAct extends AppCompatActivity implements ZBarScannerView.ResultHandler {
 
-    private ZBarScannerView zBarScannerView;
-    private boolean isFlash=false;
-    private ImageView imageView;
-    List<BarcodeFormat> formatList=new ArrayList<>();
+    private ZBarScannerView mZBarScannerView;
+    private ImageView mImageView;
+    private ActionBar mActionBar;
+    private TextView mTextView;
+    private FrameLayout mContainer;
+    private int RESET_ID;
+    private Context context;
+    private static boolean flag = false;
+    private static final int PERMISSION_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zbar);
-        FrameLayout contentFrame= (FrameLayout) findViewById(R.id.content_frame);
-        zBarScannerView=new ZBarScannerView(this);
-        contentFrame.addView(zBarScannerView);
-        imageView= (ImageView)findViewById(R.id.scan_tick);
+        mActionBar = getSupportActionBar();
+        mActionBar.setTitle(getString(R.string.actionbar_title));
+        mActionBar.setDisplayHomeAsUpEnabled(true);
+        mContainer = (FrameLayout) findViewById(R.id.container);
+        mZBarScannerView =(ZBarScannerView)findViewById(R.id.scanner_view);
+        mImageView = (ImageView) findViewById(R.id.xScanCompleate);
+        mTextView=(TextView)findViewById(R.id.title);
+        mTextView.bringToFront();
+        mZBarScannerView.setVisibility(View.INVISIBLE);
+        context=getApplicationContext();
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (isCameraPermissionGranted()) {
+                initiateScan();
+            } else {
+                checkCameraPermission();
+            }
+        }
+        else
+        {
+            initiateScan();
+        }
+    }
+    @TargetApi(Build.VERSION_CODES.M)
+    private void checkCameraPermission() {
+        if(!flag){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                Snackbar.make(mContainer, "You must enable Camera to continue scanning", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("Ok", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                ActivityCompat.requestPermissions(ZbarAct.this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CODE);
+                            }
+                        }).show();
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CODE);
+            }
+        } else {
+            Snackbar.make(mContainer, "You must enable Camera to continue scanning", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Ok", new View.OnClickListener() {
 
+                        @Override
+                        public void onClick(View v) {
+                            final Intent i = new Intent();
+                            i.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                            i.addCategory(Intent.CATEGORY_DEFAULT);
+                            i.setData(Uri.parse("package:" + context.getPackageName()));
+                            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                            i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                            startActivity(i);
+                        }}).show();
+        }
     }
 
+    private void initiateScan() {
+        mZBarScannerView.setVisibility(View.VISIBLE);
+        mZBarScannerView.setupScanner();
+        mImageView.setVisibility(View.INVISIBLE);
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private boolean isCameraPermissionGranted() {
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if(grantResults.length > 0 ) {
+            if (requestCode == PERMISSION_REQUEST_CODE && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                initiateScan();
+            } else {
+                if(!flag){
+                    final AlertDialog builder=new AlertDialog.Builder(ZbarAct.this).create();
+                    builder.setTitle("Permission Denied");
+                    builder.setMessage("You must grant Camera permission to enable Scaning.");
+                    builder.setButton(DialogInterface.BUTTON_POSITIVE,"Allow", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(ZbarAct.this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CODE);
+                        }
+                    });
+                    builder.setButton(DialogInterface.BUTTON_NEGATIVE,"Deny", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            builder.dismiss();
+                            flag = true;
+                        }
+                    });
+                    builder.show();
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        if(menuItem.getItemId() == android.R.id.home ) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            return true;
+        } else if(menuItem.getItemId() == RESET_ID) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mImageView.setVisibility(View.INVISIBLE);
+                }
+            }, 3000);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(menuItem);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+        MenuItem reset= menu.add("RESET");
+        reset.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        RESET_ID=reset.getItemId();
+        return true;
+    }
 
     @Override
     public void onResume() {
         super.onResume();
-        zBarScannerView.setResultHandler(this);
-        zBarScannerView.startCamera();
+        mZBarScannerView.setResultHandler(this);
+        mZBarScannerView.startCamera();
+        if (Build.VERSION.SDK_INT >= 23)
+         if(isCameraPermissionGranted())
+                initiateScan();
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        zBarScannerView.stopCamera();
+        mZBarScannerView.stopCamera();
     }
 
     @Override
     public void handleResult(Result result) {
-        zBarScannerView.stopCamera();
+        mZBarScannerView.stopCamera();
     if(result!=null){
-
-        imageView.setVisibility(View.VISIBLE);
-        imageView.bringToFront();
+        mImageView.setVisibility(View.VISIBLE);
+        mImageView.bringToFront();
         Toast.makeText(this,result.getContents(),Toast.LENGTH_LONG).show();
-
-    }
-        else
-        {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mImageView.setVisibility(View.INVISIBLE);
+                mZBarScannerView.startCamera();
+            }
+        }, 3000);
+    } else {
             Toast.makeText(this,"Null Result",Toast.LENGTH_LONG).show();
         }
-
-
     }
 }
--- app/src/main/java/com/example/divyasethi/qrscanner/Zxing.java
+++ app/src/main/java/com/example/divyasethi/qrscanner/Zxing.java
@@ -1,107 +1,212 @@
 package com.example.divyasethi.qrscanner;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.CountDownTimer;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Handler;
+import android.provider.Settings;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
 import com.google.zxing.ResultPoint;
 import com.journeyapps.barcodescanner.BarcodeCallback;
 import com.journeyapps.barcodescanner.BarcodeResult;
 import com.journeyapps.barcodescanner.DecoratedBarcodeView;
-
 import java.util.List;
 
+public class Zxing extends AppCompatActivity {
+    private DecoratedBarcodeView mBarcodeView;
+    private ImageView mImageView;
+    private ActionBar mActionBar;
+    private static final int PERMISSION_REQUEST_CODE = 1;
+    private FrameLayout mContainer;
+    private static boolean available = true;
+    private static boolean flag = false;
+    private Context context;
+    private int RESET_ID;
 
-public class Zxing extends AppCompatActivity{
-
-    private DecoratedBarcodeView barcodeView;
-    private ImageView imageView;
-    private ActionBar actionBar;
-    private static final int PERMISSION_REQUEST_CODE=1;
-
-    private BarcodeCallback barcodeCallback= new BarcodeCallback() {
+    private BarcodeCallback mCallback = new BarcodeCallback() {
         @Override
         public void barcodeResult(BarcodeResult result) {
-
-            Toast.makeText(getApplicationContext(),result.getText(),Toast.LENGTH_SHORT).show();
-            imageView=(ImageView)findViewById(R.id.scan_tick1);
-            imageView.setVisibility(View.VISIBLE);
-            imageView.bringToFront();
+            Toast.makeText(getApplicationContext(), result.getText(), Toast.LENGTH_SHORT).show();
+            mImageView = (ImageView) findViewById(R.id.xScanCompleate);
+            mImageView.setVisibility(View.VISIBLE);
+            mImageView.bringToFront();
+            mBarcodeView.pause();
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mImageView.setVisibility(View.INVISIBLE);
+                    mBarcodeView.resume();
+                }
+            }, 3000);
         }
 
         @Override
         public void possibleResultPoints(List<ResultPoint> resultPoints) {
-
         }
     };
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zxing);
-        actionBar=getSupportActionBar();
-        actionBar.setTitle("Scan Shipment");
-        barcodeView= (DecoratedBarcodeView)findViewById(R.id.decorated);
-        if(!(ContextCompat.checkSelfPermission(getApplicationContext(),"android.permission.CAMERA")== PackageManager.PERMISSION_DENIED)) {
-
-            Toast.makeText(getApplicationContext(),"Camera available",Toast.LENGTH_SHORT).show();
-            scan();
+        mImageView = (ImageView) findViewById(R.id.xScanCompleate);
+        mBarcodeView = (DecoratedBarcodeView) findViewById(R.id.xScannerView);
+        mContainer = (FrameLayout) findViewById(R.id.container);
+        context = getApplicationContext();
+        mActionBar = getSupportActionBar();
+        mActionBar.setTitle(getString(R.string.actionbar_title));
+        mActionBar.setDisplayHomeAsUpEnabled(true);
+        mBarcodeView.setVisibility(View.INVISIBLE);
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (isCameraPermissionGranted()) {
+                initiateScan();
+            } else {
+                checkCameraPermission();
+            }
         }
-          else  {
-                barcodeView.setVisibility(View.INVISIBLE);
-                String[] permissions= new String[1];
-                permissions[0]= new String("android.permission.CAMERA");
-                ActivityCompat.requestPermissions(this,permissions,PERMISSION_REQUEST_CODE);
-
+        else
+        {
+            initiateScan();
         }
-
     }
 
-    private void scan(){
+    @TargetApi(Build.VERSION_CODES.M)
+    private void checkCameraPermission() {
+        if(!flag){
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+            Snackbar.make(mContainer, "You must enable Camera to continue scanning", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Ok", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            ActivityCompat.requestPermissions(Zxing.this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CODE);
+                        }
+                    }).show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CODE);
+        }
+    } else {
+        Snackbar.make(mContainer, "You must enable Camera to continue scanning", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("Ok", new View.OnClickListener() {
 
+                            @Override
+                            public void onClick(View v) {
+                            final Intent i = new Intent();
+                            i.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                            i.addCategory(Intent.CATEGORY_DEFAULT);
+                            i.setData(Uri.parse("package:" + context.getPackageName()));
+                            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                            i.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                            startActivity(i);
+                         }}).show();
+        }
+    }
 
-        barcodeView.setStatusText("");
-        barcodeView.removeViewAt(1);
-        barcodeView.decodeContinuous(barcodeCallback);
+    private void initiateScan() {
+        mBarcodeView.setVisibility(View.VISIBLE);
+        mBarcodeView.setStatusText("");
+        mBarcodeView.removeViewAt(1);
+        mImageView.setVisibility(View.INVISIBLE);
+        mBarcodeView.decodeContinuous(mCallback);
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private boolean isCameraPermissionGranted() {
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            return false;
+        }
+        return true;
     }
 
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if(grantResults.length > 0 ) {
+            if (requestCode == PERMISSION_REQUEST_CODE && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                initiateScan();
+            } else {
+                    if(!flag){
+                    final AlertDialog builder=new AlertDialog.Builder(Zxing.this).create();
+                    builder.setTitle("Permission Denied");
+                    builder.setMessage("You must grant Camera permission to enable Scaning.");
+                    builder.setButton(DialogInterface.BUTTON_POSITIVE,"Allow", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(Zxing.this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CODE);
+                        }
+                    });
+                    builder.setButton(DialogInterface.BUTTON_NEGATIVE,"Deny", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            builder.dismiss();
+                            flag=true;
+                        }
+                    });
+                    builder.show();
+            }
+          }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuItem reset = menu.add("RESET");
+        reset.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        RESET_ID = reset.getItemId();
+        return true;
+    }
+
+    @Override
     protected void onPause() {
         super.onPause();
-        barcodeView.pause();
+        if (available) {
+            mBarcodeView.pause();
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        barcodeView.resume();
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        Log.e("Inside onRequest",Integer.toString(requestCode)+Integer.toString(PERMISSION_REQUEST_CODE));
-        switch (requestCode) {
-            case PERMISSION_REQUEST_CODE:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    Toast.makeText(getApplicationContext(), "Permission Granted, restart app to start scanning", Toast.LENGTH_LONG).show();
-
-                } else {
-
-                    Toast.makeText(getApplicationContext(), "Permission Denied, You cannot scan.", Toast.LENGTH_LONG).show();
-
-                }
-                break;
+        if (Build.VERSION.SDK_INT >= 23)
+            if(isCameraPermissionGranted())
+                initiateScan();
+        if (available) {
+            mBarcodeView.resume();
         }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        if (menuItem.getItemId() == android.R.id.home) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            return true;
+        } else if (menuItem.getItemId() == RESET_ID) {
+                    mImageView.setVisibility(View.INVISIBLE);
+                    mBarcodeView.resume();
+                    return true;
+                }
+         else {
+            return super.onOptionsItemSelected(menuItem);
+        }
+    }
 }
+
